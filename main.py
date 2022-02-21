8261b2b6c6f6d5a28eda8abfd7395b88394286f8
Christine Johnson <49726302+christinej697@users.noreply.github.com>
2022-02-21 15:40:50 -0700

    Worked on stuff

--- main.py
+++ main.py
@@ -4,13 +4,34 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import numpy as np
 
+def RNA(b_string):
+    #convert b_string to array of b1 to bn {AUCG} characters
+    b_arr=list(b_string)
+    n = len(b_arr)
+    if {b_arr[1], b_arr[7]} == ({'A', 'U'}):
+        print("works")
+    #create matrix to store subproblem solutions
+    M = np.zeros((n,n),dtype=int)
+    print(f'{np.array(M)}')
+    #Initialize M(i,j)=0 whenever i >= j-4
+    for k in range(5, n):
+        for i in range (1,k):
+            j=i+k
+            if i >= (j-4):
+                M[i,j] = 0
+            #check if b_j is involved in a pair
+            #if (b_arr[i] == 'A' && b_arr[j] == 'C') || (b_arr[i] == 'C' && b_arr[j] == 'A') || (b_arr[i] == 'U' && b_arr[j] == 'C') || (b_arr[i] == 'A' && b_arr[j] == 'C'):
+            #M(i,j)=
+    return M[1,n]
+
+
+def recover():
+    print("hi")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    RNA('AACGCGUU')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
