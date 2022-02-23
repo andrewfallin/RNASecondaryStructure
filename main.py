5f0e888b023939f203758e8a3aef4da3de335340
Christine Johnson <49726302+christinej697@users.noreply.github.com>
2022-02-22 19:39:18 -0700

    Attempt at memoization. Prints correct values for number of pairings

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 import numpy as np
-
+'''
+import numPy as np
 def RNA(b_string):
     #convert b_string to array of b1 to bn {AUCG} characters
     b_arr=list(b_string)
@@ -25,6 +24,49 @@ def RNA(b_string):
             #if (b_arr[i] == 'A' && b_arr[j] == 'C') || (b_arr[i] == 'C' && b_arr[j] == 'A') || (b_arr[i] == 'U' && b_arr[j] == 'C') || (b_arr[i] == 'A' && b_arr[j] == 'C'):
             #M(i,j)=
     return M[1,n]
+'''
+def get_Pairing(i, j, myArray):
+
+    j_Char = myArray[j]
+    maxT = i
+    for t in range(i, j-1):
+        t_Char = myArray[t]
+        if j_Char == 'A' and t_Char == 'U':
+            maxT = t
+        elif j_Char == 'C' and t_Char == 'G':
+            maxT = t
+        elif j_Char == 'U' and t_Char == 'A':
+            maxT = t
+        elif j_Char == 'G' and t_Char == 'C':
+            maxT = t
+    return maxT
+
+def RNA_Recursion(i, j, myArray):
+
+    if i >= j-4:
+        return 0
+    else:
+        t = get_Pairing(i, j, myArray)
+        num1 = RNA_Recursion(i, j - 1, myArray)
+        num2 = 1 + RNA_Recursion(i, t-1, myArray) + RNA_Recursion(t+1, j, myArray)
+        return max(num1, num2)
+
+
+def RunRNA(myString):
+    myArray = list(myString.upper())
+    OPT = np.empty((len(myArray),len(myArray)))
+    for i in range(len(myArray)):
+        for j in range(len(myArray)):
+            #thing.append(RNA_Recursion(i, j, myArray))
+            OPT[i][j] = RNA_Recursion(i, j, myArray)
+
+    #store t's here to be space efficient
+    for i in range(len(myArray)):
+        for j in range(0,i):
+            OPT[i][j] = OPT[j][i]
+
+    # Pseudocode OPT(1,n)
+    return OPT[0][len(myArray)-1]
 
 
 def recover():
@@ -32,6 +74,9 @@ def recover():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    RNA('AACGCGUU')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # RNA('AACGCGUU')
+    print(RunRNA('AACGCGUU'))
+    print(RunRNA('ggggaaaacccc'))
+    print(RunRNA('gggggaaaaccccc'))
+    print(RunRNA('acaugauggccaugu'))
+    print(RunRNA('cagaucggcgauacgagcauagcaaugcuaagcgagcuuagcugca'))
